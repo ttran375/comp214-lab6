e8414f6cb601394259f2aae3e0e9a47f5889ec41
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-27 15:39:40 +0000

    Tue, Feb 27, 2024, 3:39 PM +00:00

--- assignment-05.sql
+++ assignment-05.sql
@@ -5,19 +5,34 @@
 -- needs to be added to the block that displays the message “Invalid shopper ID” onscreen. Use
 -- an initialized variable named lv_shopper_num to provide a shopper ID. Test the block with the
 -- shopper ID 99.
+-- Declare a block
 DECLARE
+  -- Declare a record variable of type 'bb_shopper'
   rec_shopper    bb_shopper%ROWTYPE;
+  
+  -- Declare a variable to hold the shopper ID and initialize it to 99
   lv_shopper_num bb_shopper.idShopper%TYPE := 99;
+
+-- Begin the execution block
 BEGIN
+  -- Begin an inner block
   BEGIN
+    -- Select all columns from the 'bb_shopper' table where 'idShopper' equals the value of 'lv_shopper_num'
+    -- Store the result in the 'rec_shopper' record
     SELECT
       * INTO rec_shopper
     FROM
       bb_shopper
     WHERE
       idShopper = lv_shopper_num;
+      
+  -- Handle exceptions
   EXCEPTION
+    -- If no data was found (i.e., there is no shopper with the ID stored in 'lv_shopper_num'), then handle the exception
     WHEN NO_DATA_FOUND THEN
+      -- Output a message indicating that the shopper ID is invalid
       DBMS_OUTPUT.PUT_LINE('Invalid shopper ID');
+  -- End the inner block
   END;
+-- End the execution block
 END;
