acc1afdc25f3945e9c05a18e26749e92c0004923
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-27 11:14:41 +0000

    Tue, Feb 27, 2024, 11:14 AM +00:00

--- case_projects-02.sql
+++ case_projects-02.sql
@@ -13,8 +13,8 @@ ALTER TABLE mm_movie ADD STK_FLAG VARCHAR(1);
 
 -- Create an anonymous PL/SQL block to update the STK_FLAG column
 DECLARE
-  v_movie_id    mm_movie.movie_id%TYPE;
-  v_movie_value mm_movie.movie_value%TYPE;
+  lv_movie_id    mm_movie.movie_id%TYPE;
+  lv_movie_value mm_movie.movie_value%TYPE;
 BEGIN
  -- Cursor to fetch movie_id and movie_value
   FOR movie_rec IN (
@@ -24,21 +24,21 @@ BEGIN
     FROM
       mm_movie
   ) LOOP
-    v_movie_id := movie_rec.movie_id;
-    v_movie_value := movie_rec.movie_value;
+    lv_movie_id := movie_rec.movie_id;
+    lv_movie_value := movie_rec.movie_value;
  -- Update STK_FLAG based on the condition
-    IF v_movie_value >= 75 THEN
+    IF lv_movie_value >= 75 THEN
       UPDATE mm_movie
       SET
         STK_FLAG = '*'
       WHERE
-        movie_id = v_movie_id;
+        movie_id = lv_movie_id;
     ELSE
       UPDATE mm_movie
       SET
         STK_FLAG = NULL
       WHERE
-        movie_id = v_movie_id;
+        movie_id = lv_movie_id;
     END IF;
   END LOOP;
 
