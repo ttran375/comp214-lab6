f5b0ed6954787aebad2379dd2dce243c84d5d63a
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-27 12:03:32 +0000

    Tue, Feb 27, 2024, 12:03 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,21 +1,18 @@
--- Assignment 4-5: Handling Predefined Exceptions
--- A block of code has been created to retrieve basic customer information (see the
--- assignment04-05.sql file in the Chapter04 folder). The application page was modified so
--- that an employee can enter a customer number that could cause an error. An exception handler
--- needs to be added to the block that displays the message “Invalid shopper ID” onscreen. Use
--- an initialized variable named lv_shopper_num to provide a shopper ID. Test the block with the
--- shopper ID 99.
-DECLARE
-  rec_shopper bb_shopper%ROWTYPE;
-  lv_shopper_num bb_shopper.idShopper%TYPE := 99; -- Initialize lv_shopper_num with the shopper ID
+ALTER TABLE bb_basketitem ADD CONSTRAINT bitems_qty_ck CHECK (quantity < 20);
+
 BEGIN
   BEGIN
-    SELECT *
-    INTO rec_shopper
-    FROM bb_shopper
-    WHERE idShopper = lv_shopper_num;
+    INSERT INTO bb_basketitem VALUES (
+      88,
+      8,
+      10.8,
+      21,
+      16,
+      2,
+      3
+    );
   EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-      DBMS_OUTPUT.PUT_LINE('Invalid shopper ID');
+    WHEN VALUE_ERROR THEN
+      DBMS_OUTPUT.PUT_LINE('Check Quantity');
   END;
 END;
